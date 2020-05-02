4322aed11736b063b3d65fbdfec5fbc15715e191
Willian Reis <willian.fagoc@gmail.com>
2020-05-02 20:21:02 -0300

    Tira fotografia a partir da camera

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -6,6 +6,7 @@ export default class CameraController {
         navigator.mediaDevices.getUserMedia({
             video: true
         }).then(stream => {
+            this._stream = stream
             //Cria url do vídeo
             //this._videoEl.src = URL.createObjectURL(stream) //deprecated
             this._videoEl.srcObject = stream;
@@ -13,5 +14,22 @@ export default class CameraController {
         }).catch(err => {
             console.error(err)
         })
+    }
+
+    stop(params) {
+        this._stream.getTracks().forEach(track => {
+            track.stop()
+        })        
+    }
+
+    takePicture(mimeType = 'image/png') {
+        let canvas = document.createElement('canvas')
+        canvas.setAttribute('height', this._videoEl.videoHeight)
+        canvas.setAttribute('width', this._videoEl.videoWidth)
+
+        let context = canvas.getContext('2d')
+        context.drawImage(this._videoEl, 0, 0, canvas.width, canvas.height)
+
+        return canvas.toDataURL(mimeType)
     }
 }
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -173,10 +173,31 @@ export default class WhatsAppController {
 
         this.el.btnClosePanelCamera.on('click', e => {
             this.showMessages()
+            this._camera.stop()
+            this.el.btnReshootPanelCamera.click()
         })
 
         this.el.btnTakePicture.on('click', e => {
-            console.log('Take Picture')
+            let dataUrl = this._camera.takePicture()
+
+            this.el.pictureCamera.src = dataUrl
+            this.el.pictureCamera.show()
+            this.el.videoCamera.hide()
+            this.el.btnReshootPanelCamera.show()
+            this.el.containerTakePicture.hide()
+            this.el.containerSendPicture.show()
+        })
+
+        this.el.btnReshootPanelCamera.on('click', e => {
+            this.el.pictureCamera.hide()
+            this.el.videoCamera.show()
+            this.el.btnReshootPanelCamera.hide()
+            this.el.containerTakePicture.show()
+            this.el.containerSendPicture.hide()
+        })
+
+        this.el.containerSendPicture.on('click', e => {
+            console.log(this.el.pictureCamera.src)
         })
 
         this.el.btnSendDocument.on('click', e => {
