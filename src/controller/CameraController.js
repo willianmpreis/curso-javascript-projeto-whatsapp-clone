77c22f39a9f9622efdbd4ef05f09237f1632701d
Willian Reis <willian.fagoc@gmail.com>
2020-05-02 18:37:32 -0300

    Corrige problema na camera

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -1,4 +1,4 @@
-class CameraController {
+export default class CameraController {
     constructor(videoEl) {
         this._videoEl = videoEl
 
@@ -7,7 +7,8 @@ class CameraController {
             video: true
         }).then(stream => {
             //Cria url do vídeo
-            this._videoEl.src = URL.createObjectURL(stream)
+            //this._videoEl.src = URL.createObjectURL(stream) //deprecated
+            this._videoEl.srcObject = stream;
             this._videoEl.play()
         }).catch(err => {
             console.error(err)
