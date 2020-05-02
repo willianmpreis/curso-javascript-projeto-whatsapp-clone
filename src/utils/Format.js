0fa036dbfce628bc6c16789bd09a6a53ebc4414a
Willian Reis <willian.fagoc@gmail.com>
2020-05-02 15:31:15 -0300

    Formatar milisegundos em segundos e minutos

--- src/utils/Format.js
+++ src/utils/Format.js
@@ -6,4 +6,18 @@ class Format {
 
         return Object.keys(div.firstChild.dataset)[0]
     }
+
+    static toTime(duration) {
+        let seconds = parseInt((duration/1000)%60)
+        let minutes = parseInt((duration/(1000*60))%60)
+        let hours = parseInt((duration/(1000*60*60))%24)
+
+        seconds = seconds.toString().padStart(2, '0')
+        minutes = minutes.toString().padStart(2, '0')
+
+        if(hours > 0) {
+            return `${hours}:${minutes}:${seconds}`
+        }
+        return `${minutes}:${seconds}`
+    }
 }
