7de279322c36b91c5ec028e97f0d68ada4dfda4f
Willian Reis <willian.fagoc@gmail.com>
2020-05-02 11:47:23 -0300

    Add eventos abrir/fechar Paineis Contato e Perfil

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -3,6 +3,7 @@ class WhatsAppController {
         console.log("WhatsAppController ok!")
         this.elementsPrototype()
         this.loadElements()
+        this.initEvents()
     }
 
     loadElements() {
@@ -60,5 +61,35 @@ class WhatsAppController {
         Element.prototype.hasClass = function(name) {
             return this.classList.contains(name)
         }
+    }
+
+    initEvents() {
+        this.el.myPhoto.on('click', e => {
+            this.closeAllLeftPanel()
+            this.el.panelEditProfile.show()
+            setTimeout(() => {
+                this.el.panelEditProfile.addClass('open')
+            }, 300)
+        })
+
+        this.el.btnNewContact.on('click', e => {
+            this.closeAllLeftPanel()
+            this.el.panelAddContact.show()
+            setTimeout(() => {
+                this.el.panelAddContact.addClass('open')
+            }, 300)
+        })
+        this.el.btnClosePanelEditProfile.on('click', e => {
+            this.el.panelEditProfile.removeClass('open')
+        })
+
+        this.el.btnClosePanelAddContact.on('click', e => {
+            this.el.panelAddContact.removeClass('open')
+        })
+    }
+
+    closeAllLeftPanel() {
+        this.el.panelAddContact.hide()
+        this.el.panelEditProfile.hide()
     }
 }
