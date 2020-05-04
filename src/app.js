62f14768aa85c0029e54f1f313cb920a919dc82a
Willian Reis <willian.fagoc@gmail.com>
2020-05-04 17:29:16 -0300

    Adiciona arquivo firebaseConfig com configuracoes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 import WhatsAppController from './controller/WhatsAppController'
 
 window.app = new WhatsAppController();
--- src/db/Firebase.js
+++ src/db/Firebase.js
@@ -1,18 +1,10 @@
 const firebase = require('firebase')
 require('firebase/firestore')
+import {firebaseConfig} from './firebaseConfig'
 
 export class Firebase {
     constructor() {
-         this._firebaseConfig = {
-             apiKey: process.env.APIKEY,
-             authDomain: process.env.AUTHDOMAIN,
-             databaseURL: process.env.DATABASEURL,
-             projectId: process.env.PROJECTID,
-             storageBucket: process.env.STORAGEBUCKET,
-             messagingSenderId: process.env.MESSAGINGSENDERID,
-             appId: process.env.APPID,
-             measurementId: process.env.MEASUREMENTID
-           };
+        this._firebaseConfig = firebaseConfig;
         
         this.init()
     }
